c63fa8d4748510e161827d1331b5b6433aeec4ea
osoriomica <osoriomica@gmail.com>
2025-06-14 13:52:54 +0100

    Complete code from kaboom doc

--- game/static/js/game.js
+++ game/static/js/game.js
@@ -4,18 +4,20 @@ kaboom({
     font: "monospace",
     scale: 1,
     background: [136, 136, 223],
-
+    height: 700,
+    width: 700,
+    letterbox: true,
 });
 
 // Load assets
 // Sprites
 loadRoot('https://i.imgur.com/');
-loadSprite('coin', 'WbKxhcd.png');
+loadSprite('coin', 'wbKxhcd.png');
 loadSprite('goomba', 'KPO3fR9.png');
 loadSprite('brick', 'pogC9x5.png');
 loadSprite('block', 'M6rwarW.png');
 loadSprite('mario', 'Wb1qfhK.png');
-loadSprite('mushroom', 'OwMd92p.png');
+loadSprite('mushroom', '0wMd92p.png');
 loadSprite('surprise', 'gesQ1KP.png');
 loadSprite('unboxed', 'bdrLpi6.png');
 loadSprite('pipe-top-left', 'ReTPiWY.png');
@@ -63,8 +65,10 @@ function big() {
         update() {
             if (isBig) {
                 timer -= dt()
+                CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
                 if (timer <= 0) {
                     this.smallify()
+                    CURRENT_JUMP_FORCE = JUMP_FORCE
                 }
             }
             this.scale = this.scale.lerp(vec2(destScale), dt() * 6)
@@ -74,12 +78,12 @@ function big() {
             return isBig
         },
         smallify() {
-            destScale = 1
+            destScale = 2 
             timer = 0
             isBig = false
         },
         biggify(time) {
-            destScale = 2
+            destScale = 4
             timer = time
             isBig = true
         },
@@ -88,14 +92,11 @@ function big() {
 
 // Constants for Speed
 const MOVE_SPEED = 240;
-const JUMP_FORCE = 700;
-const BIG_JUMP_FORCE = 1000;
+const JUMP_FORCE = 900;
+const BIG_JUMP_FORCE = 1300;
 let CURRENT_JUMP_FORCE = JUMP_FORCE;
 const FALL_DEATH = 800;
 const ENEMY_SPEED = 60;
-
-// Game Logic
-let isJumping = true;
 
 const LEVELS = [
     [
@@ -113,18 +114,18 @@ const LEVELS = [
         '====================================================================   ===========',
     ],
     [
-        '£.()............................................................................................................£',
-        '£.-+............................................................................................................£',
-        '£...............................................................................................................£',
-        '£...............................................................................................................£',
-        '£...............................................................................................................£',
-        '£...............................................................................................................£',
-        '£...............................................................................................................£',
-        '£................................................x..............................................................£',
-        '£................................................x............................................................-+£',
-        '£...............@@@@@........................x...x.....x..................................................-+..()£',
-        '£.................................z.................................................z.....................()..()£',
-        '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+        '£.().......................................................................................£',
+        '£.-+.......................................................................................£',
+        '£..........................................................................................£',
+        '£..........................................................................................£',
+        '£..........................................................................................£',
+        '£..........................................................................................£',
+        '£..........................................................................................£',
+        '£................@@@@............................x.........................................£',
+        '£................................................x.......................................-+£',
+        '£............................................x...x.....x.............................-+..()£',
+        '£.............................x....z.........x...x.....x.......z.....................()..()£',
+        '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     ],
 ]
 
@@ -151,7 +152,7 @@ const levelConf = {
     offscreen({ hide: true }), 'pipe'],
         '+': () => [sprite('pipe-top-right'), area(), body({ isStatic: true }), anchor("bot"),
     offscreen({ hide: true }), 'pipe'],
-        '^': () => [sprite('goomba'), area(), body({ isStatic: true }), scale(2), anchor("bot"),
+        '^': () => [sprite('goomba'), area(), body(), scale(2), anchor("bot"),
     offscreen({ hide: true }), 'enemy', goombaMoves()],
         '#': () => [sprite('mushroom'), area(), body(), scale(2), anchor("bot"),
     offscreen({ hide: true }), 'mushroom'],
@@ -159,7 +160,7 @@ const levelConf = {
     offscreen({ hide: true }), "platform",],
         '£': () => [sprite('blue-brick'), area(), body({ isStatic: true }), anchor("bot"),
     offscreen({ hide: true })],
-        'z': () => [sprite('blue-goomba'), area(), body({ isStatic: true }), anchor("bot"),
+        'z': () => [sprite('blue-goomba'), area(), body(), anchor("bot"),
     offscreen({ hide: true }), 'enemy', goombaMoves()],
         '@': () => [sprite('blue-surprise'), area(), body({ isStatic: true }), anchor("bot"),
     offscreen({ hide: true }), 'coin-surprise'],
@@ -168,26 +169,14 @@ const levelConf = {
     },
 };
 
-scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
+scene("game", ({ levelId, coins } = { levelId: 0, coins: 0 }) => {
 
     //add level to scene
     const level = addLevel(LEVELS[levelId ?? 0], levelConf);
 
-    // add score label  - ANIAS CODE
-    const scoreLabel = add([
-        text(`score: ${score}`, { size: 16, color: [255, 255, 255, 1] }),
-        pos(90, 300),
-        z(100),
-
-    ])
-
-    // Converts the string value to an integer to display correct level 
-    // to the user, next to the score.
-    add([text('level' + parseInt(levelId + 1), { size: 16 }), pos(10, 300)])
-
     const player = add([
         sprite('mario'),
-        pos(30, 0),
+        pos(100, 90),
         area(),
         body(),
         anchor('bot'),
@@ -200,12 +189,12 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
     player.onUpdate(() => {
         camPos(player.pos)
         if (player.pos.y >= FALL_DEATH) {
-            go('lose', { score: scoreLabel.value })
+            go('lose', { score: coinsLabel })
         }
     })
 
     player.onBeforePhysicsResolve((collision) => {
-        if (collision.target.is(["platform", "soft"]) && player.isJumping()) {
+        if (collision.target.is(["platform"]) && player.isJumping()) {
             collision.preventResolution()
         }
     })
@@ -217,8 +206,8 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
 
     // if player onCollide with any obj with "danger" tag, lose
     player.onCollide("danger", () => {
-        go("lose")
-        play("hit")
+        go("lose", { score: coinsLabel.text })
+        // play("sfx")
     })
 
     player.onCollide("pipe", () => {
@@ -227,7 +216,7 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
             if (levelId + 1 < LEVELS.length) {
                 go("game", {
                     levelId: levelId + 1,
-                    score: scoreLabel.value,
+                    coins: coins,
                 })
             } else {
                 go("win")
@@ -247,13 +236,77 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
     player.onCollide("enemy", (e, col) => {
 		// if it's not from the top, die
 		if (!col.isBottom()) {
-			go("lose")
+			go("lose", { score: coinsLabel.text })
 			// play("sfx")
 		}
 	})
 
-    let hasMushroom = false
+	// grows mushroom if player's head bumps into an obj with "mushroom-surprise" tag
+	player.onHeadbutt((obj) => {
+		if (obj.is("mushroom-surprise")) {
+			const mushroom = level.spawn("#", obj.tilePos.sub(0, 1))
+			mushroom.jump()
+            destroy(obj)
+            level.spawn("}", obj.tilePos.add(0,0))
+            mushroom.move()
+			// play("sfx")
+		}
+	})
 
+	// player grows big onCollide with a "Mushroom" obj
+	player.onCollide("mushroom", (m) => {
+		destroy(m)
+		// as we defined in the big() component
+		player.biggify(5)
+		// play("sfx")
+	})
+
+    // releases coin if player's head bumps into an obj with "coin-surprise" tag
+    // then changes surprise box to unboxed - from Ania Kubow
+	player.onHeadbutt((obj) => {
+		if (obj.is("coin-surprise")) {
+			level.spawn("$", obj.tilePos.sub(0, 1))
+            destroy(obj)
+            level.spawn("}", obj.tilePos.add(0,0))
+			// play("sfx")
+		}
+	})
+
+    // Collect coins, update score
+    let coinPitch = 0
+
+	onUpdate(() => {
+		if (coinPitch > 0) {
+			coinPitch = Math.max(0, coinPitch - dt() * 100)
+		}
+	})
+
+	player.onCollide("coin", (c) => {
+		destroy(c)
+		// play("sfw")
+		coinPitch += 100
+		coins += 1
+		coinsLabel.text =  "SCORE: " + coins
+	})
+
+    // add score label 
+    const coinsLabel = add([
+        text("SCORE: " + coins),
+        pos(40, 160),
+        {value: coins },
+        z(100),
+        fixed()
+    ])
+
+    // Converts the string value to an integer to display correct level 
+    // to the user, next to the score.
+    const levelLabel = add([
+        text("LEVEL - " + parseInt(levelId + 1)),
+        pos(40, 90), 
+        fixed(),
+    ])
+
+    // Player Moves
     onKeyDown('left', () => {
         player.move(-MOVE_SPEED, 0)
     })
@@ -262,18 +315,23 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
         player.move(MOVE_SPEED, 0)
     })
 
-    player.onUpdate(() => {
-        if (player.isGrounded()) {
-            isJumping = false
-        }
-    })
-
-    onKeyPress('space', () => {
+    function jump() {
         if (player.isGrounded()) {
             player.jump(CURRENT_JUMP_FORCE)
-            isJumping = true
         }
-    })
+    }
+
+    onKeyPress('space', jump)
+
+    onGamepadButtonPress("south", jump)
+
+	onGamepadStick("left", (v) => {
+		player.move(v.x * MOVE_SPEED, 0)
+	})
+
+	onKeyPress("f", () => {
+		setFullscreen(!isFullscreen())
+	})
 
 })
 
@@ -283,9 +341,9 @@ scene("game", ({ levelId, score } = { levelId: 0, score: 0 }) => {
 //     score: 0
 // });
 
-scene("lose", ({ score.value }) => {
+scene("lose", ({ score }) => {
 	add([
-		text(`YOU LOST. YOUR SCORE IS:${score}`),
+		text(`YOU LOST. YOUR SCORE IS: ${score}`),
         pos(center)
 	])
 	onKeyPress(() => go("game"))
