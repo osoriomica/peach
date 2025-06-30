71156103a0b3e093d56968138b68297d80dd0b54
osoriomica <osoriomica@gmail.com>
2025-06-30 01:03:02 +0100

    Prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn peach.wsgi:application
--- peach/settings.py
+++ peach/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env  # noqa: F401
@@ -31,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',  # vs code preview
     'localhost',  # listen for stripe webhooks in production
+    'peachkaboom-132026d215d5.herokuapp.com',  # my app on heroku
 ]
 
 
@@ -124,12 +126,17 @@ WSGI_APPLICATION = 'peach.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
